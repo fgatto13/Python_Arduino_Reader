23092d866153d59a97023bb295dd57094c7746fe
fgatto13 <francescogatto2001@gmail.com>
2026-01-15 16:11:37 +0100

    added a device read test

--- python-code/Device.py
+++ python-code/Device.py
@@ -7,10 +7,12 @@ class Device:
         self.baud = baud
         self.port = port
     
-    def read_data(self):
+    def read_data(self, time) -> list | None:
         try:
             with serial.Serial(self.port, self.baud, timeout=1) as ser:
-                line = ser.readline().decode('utf-8').rstrip()
+                line = []
+                for _ in range(time):
+                    line.append(ser.readline().decode('utf-8').rstrip())    
                 return line
         except serial.SerialException as e:
             print(f"Error reading from device {self.device_id} on port {self.port}: {e}")
--- python-code/main.py
+++ python-code/main.py
@@ -1,8 +1,10 @@
 from DbManager import DbManager
+from Device import Device
 from dotenv import load_dotenv
 import serial
 import serial.tools.list_ports
 
+# part of the code adapted from stackoverflow.com/questions/53214304/python-pyserial-auto-detect-com-ports
 def find_serial_ports():
     ports = serial.tools.list_ports.comports(include_links=False)
     if not ports:
@@ -14,13 +16,19 @@ def main():
     # Initialize database manager
     db_manager = DbManager()
     db_manager.close()
-    # Search for available serial ports
-    # The following block of code was adapted from stackoverflow.com/questions/53214304/python-pyserial-auto-detect-com-ports
+    
     ports = find_serial_ports()
     if ports:
         print('Available serial ports:')
         for port in ports :
             print('Find port '+ port.device)
+        # now we create a device object
+        device = Device(device_id=1, name='INO_TEST', baud=9600, port=ports[0].device)
+        data = device.read_data(time=5)
+        if data:
+            print(f'Read data: {data}')
+        else:
+            print('No data read from device')
     else:
         print('No ports found')
 
