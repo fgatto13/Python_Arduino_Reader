eae2d0d7d638e130f67fd82a460b69ae79965459
fgatto13 <francescogatto2001@gmail.com>
2026-01-16 12:11:14 +0100

    implemented some basic communication between arduino and python to make sure that everything works.

--- python-code/Device.py
+++ python-code/Device.py
@@ -1,5 +1,6 @@
 # This class represents an arduino device connected to the system.
 import serial
+import time
 class Device:
     def __init__(self, device_id, name, baud, port):
         self.device_id = device_id
@@ -7,13 +8,20 @@ class Device:
         self.baud = baud
         self.port = port
     
-    def read_data(self, time) -> list | None:
+    def read_data(self) -> list | None:
         try:
             with serial.Serial(self.port, self.baud, timeout=1) as ser:
                 line = []
-                for _ in range(time):
-                    line.append(ser.readline().decode('utf-8').rstrip())    
-                return line
+                while True:
+                    i = input("on/off: ").strip()
+                    if i == "done":
+                        print("Exiting read loop.")
+                        return line
+                    ser.write(i.encode())
+                    time.sleep(0.5)
+                    print(ser.readline().decode('ascii'))
+                    line.append(ser.readline().decode('ascii'))    
+                    print(f"Device {self.device_id} on port {self.port} read data: {line[-1]}")
         except serial.SerialException as e:
             print(f"Error reading from device {self.device_id} on port {self.port}: {e}")
             return None
--- python-code/main.py
+++ python-code/main.py
@@ -1,6 +1,7 @@
 from DbManager import DbManager
 from Device import Device
 from dotenv import load_dotenv
+from simple_term_menu import TerminalMenu # https://github.com/IngoMeyer441/simple-term-menu
 import serial
 import serial.tools.list_ports
 
@@ -22,13 +23,18 @@ def main():
         print('Available serial ports:')
         for port in ports :
             print('Find port '+ port.device)
-        # now we create a device object
-        device = Device(device_id=1, name='INO_TEST', baud=9600, port=ports[0].device)
-        data = device.read_data(time=5)
-        if data:
-            print(f'Read data: {data}')
-        else:
-            print('No data read from device')
+        # Create a terminal menu for user to select a port
+        options = [port.device for port in ports]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        assert isinstance(menu_entry_index, int)
+        print(f"You have selected {options[menu_entry_index]}")
+        # after the port gets selected, we create a device instance with the selected port
+        device = Device(device_id=1, name='INO_TEST', baud=9600, port=options[menu_entry_index])
+        print(f'Reading data from device on port {device.port}...')
+        data = device.read_data()
+        if not data:
+            print('No data read from device.')
     else:
         print('No ports found')
 
